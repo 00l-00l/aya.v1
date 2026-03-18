f05ff8f9f7df7e24b7f48e5ec7e0ae9b596a301e
00l-00l <morn1ngstartv93@gmail.com>
2026-03-18 20:09:30 +0300

    feat: AYA v0.1 - command counter

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ aya_active = True
 user_name = input("Как вас зовут? ")
 if user_name == "Сэр":
     print(f"Добро Пожаловать! {user_name}",  "Ая v.1 активирована")
-    
+    command_count = 0
     while True:
      command = input("Введите команду ").lower() 
+     command_count = command_count + 1
      if command ==  "время":
          print("Функция времени в разработке")
 
@@ -12,6 +13,7 @@ if user_name == "Сэр":
          print("Функция погоды в разработке")
 
      elif command =="выход":
+         print("Выполнено команд:" f"{command_count}")
          print("До свидания")
          break
 
