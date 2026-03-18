b6bbcd9cc2cba182e2fc1189d6574617bc9b0ebe
00l-00l <morn1ngstartv93@gmail.com>
2026-03-18 12:55:46 +0300

    feat: AYA v0.1 - elif + f-strings

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 aya_active = True
 user_name = input("Как вас зовут? ")
 if user_name == "Сэр":
- print("Добро Пожаловать!",  "Ая v.1 активирована")
+ print(f"Добро Пожаловать! {user_name}",  "Ая v.1 активирована")
+
+elif user_name == "Напарник":
+ print(f"Добро пожаловать {user_name}",  "Ая v.1 активирована")
 
 else:
  print("Доступ Запрещён!")
