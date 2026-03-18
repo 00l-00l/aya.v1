92db473f0a5803b9446c72cefd1f254bb58ac74f
00l-00l <morn1ngstartv93@gmail.com>
2026-03-18 12:21:35 +0300

    feat: AYA v0.1 - access control

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-user_name = input("Как вас зовут? ")
 aya_active = True
-print("Добро пожаловать",user_name, "Ая v.1 активирована")
+user_name = input("Как вас зовут? ")
+if user_name == "Сэр":
+ print("Добро Пожаловать!",  "Ая v.1 активирована")
+
+else:
+ print("Доступ Запрещён!")
 print("---")
