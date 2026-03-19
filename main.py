6d3a4663916876b460a65e497f8dc8e65657ed94
00l-00l <morn1ngstartv93@gmail.com>
2026-03-19 10:51:10 +0300

    feat: AYA v0.1 - refactor to functions

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
+def hello(name):
+   print(f"Добро Пожаловать! {name}", "Ая v.1 активирована")
+
+def hundle_command(command):
+        
+       if command ==  "время":
+         print("Функция времени в разработке")
+
+       elif command =="погода":
+         print("Функция погоды в разработке")
+
+       elif command =="выход":
+         print("До свидания")
+         
+
+       else:
+         print("Команда не распознана")
+  
+
 aya_active = True
 user_name = input("Как вас зовут? ")
 if user_name == "Сэр":
-    print(f"Добро Пожаловать! {user_name}",  "Ая v.1 активирована")
-    command_count = 0
-    history = []
+    hello(user_name)
     while True:
-     command = input("Введите команду ").lower() 
-     history.append(command)
-     command_count = command_count + 1
-     if command ==  "время":
-         print("Функция времени в разработке")
-
-     elif command =="погода":
-         print("Функция погоды в разработке")
-
-     elif command =="выход":
-         print(history)
-         print("Выполнено команд:" f"{command_count}")
-         print("До свидания")
-         break
-
-     else:
-         print("Команда не распознана")
-         
-
-elif user_name == "Напарник":
-   print(f"Добро пожаловать {user_name}",  "Ая v.1 активирована")
-   
-else:
- print("Доступ Запрещён!")
-print("---")  
+     command = input("Введите команду ").lower()
+     hundle_command(command)
+     
