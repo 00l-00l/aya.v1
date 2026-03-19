6c0c7d31efc7824bf96ac1a31aabc96b039afa2d
00l-00l <morn1ngstartv93@gmail.com>
2026-03-19 16:47:53 +0300

    feat: AYA v0.1 - fix exit command

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 def hello(name):
-   print(f"Добро Пожаловать! {name}", "Ая v.1 активирована")
+    print(f"ДОБРО ПОЖАЛОВАТЬ! {name} Ая V.01 ГОТОВА К РАБОТЕ")
 
+def check_user(name):
+ if name == "сэр":
+    return(True)
+ else:
+    return(False)
+ 
 def hundle_command(command):
-        
-       if command ==  "время":
-         print("Функция времени в разработке")
-
-       elif command =="погода":
-         print("Функция погоды в разработке")
-
-       elif command =="выход":
-         print("До свидания")
-         
-
-       else:
-         print("Команда не распознана")
-  
+   if command == "время":
+      print("ФУНКЦИЯ В РАЗРАБОТКЕ ОБРАТИТЕСЬ ПОЗЖЕ")
+   elif command == "погода":
+      print("ФУНКЦИЯ В РАЗРАБОТКЕ ОБРАТИТЕСЬ ПОЗЖЕ")
+   elif command == "выход":
+      print("ДО СВИДАНИЯ!")
+      return False
+   else: 
+      print("ERROR")
 
 aya_active = True
-user_name = input("Как вас зовут? ")
-if user_name == "Сэр":
-    hello(user_name)
-    while True:
-     command = input("Введите команду ").lower()
-     hundle_command(command)
-     
+user_name = input("КАК К ВАМ ОБРАЩАТЬСЯ? ").lower()
+if check_user(user_name):
+      hello(user_name)
+      while True:
+       command = input("ВВЕДИТЕ КОМАНДУ ")
+       hundle_command(command) == False
+       break
+else:
+   print("ДОСТУП ЗАПРЕЩЁН!")
